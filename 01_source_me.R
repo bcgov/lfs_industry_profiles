2c0d0763285328062d5864ce07abaedf2aa70f38
Richard Martin <rich.p.martin@gmail.com>
2022-11-02 10:49:19 -0700

    some fixes

--- 01_source_me.R
+++ 01_source_me.R
@@ -16,15 +16,20 @@
 #' download the 4 resulting csv files (with names containing ftptemp4digNAICS and lfsstat4digNAICS) and place in directory "data"
 
 #' Note that Jan 2026 the 201620 SAS files will need to be updated to 202630.
-
+tictoc::tic()
 # libraries--------------
 library(tidyverse)
 library(lubridate)
 library(readxl)
 library(XLConnect)
 library(scales)
+library(tsibble)
+library(feasts)
+library(fable)
 # constants---------------
 ma_months <- 3 #how many months to use for smoothing the data
+accuracy_large <- 100
+accuracy_small <- .1
 # Functions--------------------
 source(here::here("R", "functions.R"))
 # Start by creating a mapping file from naics to various levels of aggregation----------------
@@ -51,6 +56,9 @@ agg <- raw_mapping %>%
   fill(high, .direction = "down") %>%
   fill(medium, .direction = "down") %>%
   select(industry, high, medium, low)
+
+write_csv(agg, here::here("out","layout.csv"))
+
 
 # get the naics for the lowest level of aggregation---------------
 low <- raw_mapping %>%
@@ -98,13 +106,27 @@ mapping <- high%>%
   full_join(low, by= "naics")%>%
   select(naics, everything())
 
+write_csv(mapping, here::here("out","mapping.csv"))
+
 # read in the data and join with mapping file to get aggregation info -------------------
 ftpt <- read_naics("ftptemp4digNAICS", ftpt)%>%
   inner_join(mapping, by = "naics")
 status <- read_naics("lfsstat4digNAICS", lf_stat)%>%
   inner_join(mapping, by = "naics")
-bound_data <- bind_rows(ftpt, status)%>%
-  filter(date < floor_date(today(), unit = "month")-weeks(2))
+bound_data <- bind_rows(ftpt, status)
+
+#data is zero padded to the end of the current year... figure out the last month from data.
+max_date <- bound_data%>%
+  group_by(date, name)%>%
+  summarize(value=mean(value))%>%
+  ungroup()%>%
+  filter(!near(value, 0))%>%
+  filter(date==max(date))%>%
+  pull(date)%>%
+  unique()
+
+bound_data <- bound_data%>%
+  filter(date <= max_date)
 
 # output file has dates as column headings... get the necessary dates-----------
 current <- format(max(bound_data$date), "%b-%y")
@@ -115,17 +137,21 @@ high_agg <- agg_level(bound_data, high)
 medium_agg <- agg_level(bound_data, medium)
 low_agg <- agg_level(bound_data, low)
 # bind the 3 levels of aggregation together then...
-all_agg_levels <- bind_rows(high_agg, medium_agg, low_agg) %>%
-  mutate(
-    data = map(data, add_vars), # add in the labour force and the unemployment rate
-    data = map(data, trail_ma, months = ma_months), # smooth the data
-    current = map(data, get_smoothed, 0), # get current value of smoothed data
-    last_month = map(data, get_smoothed, 1),
-    last_year = map(data, get_smoothed, 12),
-    current_ytd_ave = map(data, ytd_ave, 0), # year to date average of smoothed data
-    previous_ytd_ave = map(data, ytd_ave, 1)
+all_agg_levels <- bind_rows(high_agg, medium_agg, low_agg)
+
+write_rds(all_agg_levels, here::here("out","all_agg_levels.rds"))
+
+out_tbbl <- all_agg_levels%>%
+  mutate(#data = map(data, stl_smooth), #thought this might be better than simple moving average...
+        data = map(data, trail_ma, months = ma_months), # simple moving average smooth of data
+        data = map(data, add_vars),
+        current = map(data, get_smoothed, 0), # get current value of smoothed data
+        last_month = map(data, get_smoothed, 1),
+        last_year = map(data, get_smoothed, 12),
+        current_ytd_ave = map(data, ytd_ave, 0), # year to date average of smoothed data
+        previous_ytd_ave = map(data, ytd_ave, 1)
   )%>%
-  select(-data) %>%
+  select(-data)%>%
   mutate(#join all the dataframes created above for unnesting below (unnesting individually creates sparse dataframe)
     data = map2(current, last_month, full_join, by = "name"),
     data = map2(data, last_year, full_join, by = "name"),
@@ -133,11 +159,11 @@ all_agg_levels <- bind_rows(high_agg, medium_agg, low_agg) %>%
     data = map2(data, previous_ytd_ave, full_join, by = "name")
   ) %>%
   select(agg_level, data) %>%
-  unnest(data) %>%
+  unnest(data)%>%
   rename(
-    current = smoothed.x, # fix the names messed up by joins above
-    previous_month = smoothed.y,
-    previous_year = smoothed,
+    current = value.x, # fix the names messed up by joins above
+    previous_month = value.y,
+    previous_year = value,
     current_ytd_average = ytd_ave.x,
     previous_ytd_average = ytd_ave.y
   )%>%
@@ -145,17 +171,33 @@ all_agg_levels <- bind_rows(high_agg, medium_agg, low_agg) %>%
     level_change_year = current - previous_year,
     level_change_month = current - previous_month,
     level_change_ytd = current_ytd_average - previous_ytd_average,
-    percent_change_year = percent(level_change_year / previous_year, accuracy = .1),
-    percent_change_month = percent(level_change_month / previous_month, accuracy = .1),
-    percent_change_ytd = percent(level_change_ytd / previous_ytd_average, accuracy = .1),
-    current = if_else(name == "unemployment_rate", percent(current, accuracy = .1), comma(current, accuracy = 100)),
-    previous_year = if_else(name == "unemployment_rate", percent(previous_year, accuracy = .1), comma(previous_year, accuracy = 100)),
-    previous_month = if_else(name == "unemployment_rate", percent(previous_month, accuracy = .1), comma(previous_month, accuracy = 100)),
-    level_change_year = if_else(name == "unemployment_rate", percent(level_change_year, accuracy = .1), comma(level_change_year, accuracy = 100)),
-    level_change_month = if_else(name == "unemployment_rate", percent(level_change_month, accuracy = .1), comma(level_change_month, accuracy = 100)),
-    level_change_ytd = if_else(name == "unemployment_rate", percent(level_change_ytd, accuracy = .1), comma(level_change_ytd, accuracy = 100)),
-    current_ytd_average = if_else(name == "unemployment_rate", percent(current_ytd_average, accuracy = .1), comma(current_ytd_average, accuracy = 100)),
-    previous_ytd_average = if_else(name == "unemployment_rate", percent(previous_ytd_average, accuracy = .1), comma(previous_ytd_average, accuracy = 100))
+    percent_change_year = percent(level_change_year / previous_year, accuracy = accuracy_small),
+    percent_change_month = percent(level_change_month / previous_month, accuracy = accuracy_small),
+    percent_change_ytd = percent(level_change_ytd / previous_ytd_average, accuracy = accuracy_small),
+    current = if_else(name == "unemployment_rate",
+                      percent(current, accuracy = accuracy_small),
+                      comma(current, accuracy = accuracy_large)),
+    previous_year = if_else(name == "unemployment_rate",
+                            percent(previous_year, accuracy = accuracy_small),
+                            comma(previous_year, accuracy = accuracy_large)),
+    previous_month = if_else(name == "unemployment_rate",
+                             percent(previous_month, accuracy = accuracy_small),
+                             comma(previous_month, accuracy = accuracy_large)),
+    level_change_year = if_else(name == "unemployment_rate",
+                                percent(level_change_year, accuracy = accuracy_small),
+                                comma(level_change_year, accuracy = accuracy_large)),
+    level_change_month = if_else(name == "unemployment_rate",
+                                 percent(level_change_month, accuracy = accuracy_small),
+                                 comma(level_change_month, accuracy = accuracy_large)),
+    level_change_ytd = if_else(name == "unemployment_rate",
+                               percent(level_change_ytd, accuracy = accuracy_small),
+                               comma(level_change_ytd, accuracy = accuracy_large)),
+    current_ytd_average = if_else(name == "unemployment_rate",
+                                  percent(current_ytd_average, accuracy = accuracy_small),
+                                  comma(current_ytd_average, accuracy = accuracy_large)),
+    previous_ytd_average = if_else(name == "unemployment_rate",
+                                   percent(previous_ytd_average, accuracy = accuracy_small),
+                                   comma(previous_ytd_average, accuracy = accuracy_large))
   ) %>%
   left_join(agg, by = c("agg_level" = "industry"))%>% #agg is the mapping from industry to the 3 levels of aggregation
   mutate(medium = ifelse(agg_level == high, paste0("1", medium), medium)) %>%# allows high level industries to be at top of sorted medium industries.
@@ -172,7 +214,16 @@ all_agg_levels <- bind_rows(high_agg, medium_agg, low_agg) %>%
 
 # write to excel-----------------
 wb <- loadWorkbook(here::here("data", "template.xlsx")) # get the desired sheet header
-all_agg_levels%>%
+out_tbbl%>%
   mutate(walk2(data, high, write_sheet)) # replicates the template sheet and writes data to each sheet
 removeSheet(wb, "layout") # get rid of the template
 saveWorkbook(wb, here::here("out", "industry_snapshots.xlsx"))#write to file
+tictoc::toc()
+
+
+#
+#   mtcars |>
+#   as_tibble(rownames="Car name") |>
+#   pivot_longer(cols = -c(`Car name`, hp, vs, am), names_to = "Property", values_to = "Value")|>
+#   heatmap(`Car name`, Property, Value,scale = "column",cluster_rows = FALSE, cluster_columns = FALSE )
+#
