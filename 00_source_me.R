f437ca86476dcb330572738114b337e55bb82842
Rich-P-Martin <richard.martin@gov.bc.ca>
2024-01-28 14:05:19 -0800

    small changes

--- 00_source_me.R
+++ 00_source_me.R
@@ -56,8 +56,8 @@ rmarkdown::render("02_dashboard.Rmd",
 
 
 #archive input files--------
-#filesstrings::file.move(here::here("data","current", list.files(here::here("data", "current"), pattern = "RTRA")),
-#                        here::here("data", "old"), overwrite = TRUE)
+filesstrings::file.move(here::here("data","current", list.files(here::here("data", "current"), pattern = "RTRA")),
+                        here::here("data", "old"), overwrite = TRUE)
 
 
 
